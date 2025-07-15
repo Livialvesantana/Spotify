c3c44536250396ed23b7b64d219f5d13d5888969
Livialvesantana <livialvesantana@gmail.com>
2025-07-15 13:08:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const searchInput =document.getElementById('search-input');
+const resultsArtist =document.getElementById("result-artist");
+const resultPlaylist =document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = ``
+}
